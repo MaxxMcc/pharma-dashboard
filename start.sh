58ca1d53155faee9c8cf04229334d7bb9ce2542a
maxxmcc <mjm472@cornell.edu>
2025-06-30 14:43:57 -0500

    Initial commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Move into your project folder
+cd ~/Projects/pharma-dashboard || exit
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Optional: Print a welcome message
+echo "You're now in the (venv) environment. Project folder: $(pwd)"
